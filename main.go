6b94448fdcf893cfb474238d090d493034c40208
me <me@localhost>
2023-02-06 23:20:02 -0500

    --wip-- [skip ci]

--- main.go
+++ main.go
@@ -15,10 +15,13 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	lintPackages(packages)
+	err = lintPackages(packages)
+	fmt.Println(err)
+	fmt.Printf("done")
 }
 
 func lintPackages(pkgs map[string]*ast.Package) error {
+	typesInPkg := make(map[string]*ast.TypeSpec)
 	for _, pkg := range pkgs {
 		for _, f := range pkg.Files {
 			for _, decl := range f.Decls {
@@ -33,7 +36,10 @@ func lintPackages(pkgs map[string]*ast.Package) error {
 						// TODO: make this a bit smarter
 						return fmt.Errorf("package not allowed to export constants")
 					case token.TYPE:
-						continue
+						for _, spec := range genDecl.Specs {
+							typeSpec := spec.(*ast.TypeSpec)
+							typesInPkg[typeSpec.Name.Name] = typeSpec
+						}
 					case token.VAR:
 						for _, spec := range genDecl.Specs {
 							valueSpec := spec.(*ast.ValueSpec)
@@ -69,12 +75,56 @@ func lintPackages(pkgs map[string]*ast.Package) error {
 								if pkgType.TypeParams != nil {
 									return fmt.Errorf("pkg must not have any fields")
 								}
-
 							}
 						}
 					}
+				case *ast.FuncDecl:
+					funcDecl := decl.(*ast.FuncDecl)
+					if !ast.IsExported(funcDecl.Name.Name) {
+						continue
+					}
+
+					if funcDecl.Recv == nil {
+						return fmt.Errorf("function %v is exported and not a method on pkg", funcDecl.Name.Name)
+					}
+
+					if len(funcDecl.Recv.List) != 1 {
+						panic("bug with length of receiver list")
+					}
+
+					receiver := funcDecl.Recv.List[0]
+
+					if len(receiver.Names) != 1 {
+						panic("bug with length of receiver names list")
+					}
+
+					if _, ok := receiver.Type.(*ast.StarExpr); ok {
+						return fmt.Errorf("function %v declared with a pointer receiver", funcDecl.Name.Name)
+					}
+
+					recvTypeIdentifier, ok := receiver.Type.(*ast.Ident)
+					if !ok {
+						panic("recvIdentifier could not be cast")
+					}
+
+					panic(recvTypeIdentifier)
+
+					if funcDecl.Recv.List[0].Names[0].Name != "p" {
+						return fmt.Errorf("function %v should have a receiver named p", funcDecl.Name.Name)
+					}
+
+					if funcDecl.Type.TypeParams != nil {
+						return fmt.Errorf("function %v must not be generic", funcDecl.Name.Name)
+					}
+
+					for _, param := range funcDecl.Type.Params.List {
+						t := param.Type
+						panic(t)
+
+					}
+
 				default:
-					fmt.Printf("skipped\n")
+					panic("unhandled declaration type")
 				}
 			}
 		}
