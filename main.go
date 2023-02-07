b106546fc44522e4d1116f24bbb5aeb291ce7a08
me <me@localhost>
2023-02-06 22:32:32 -0500

    --wip-- [skip ci]

--- main.go
+++ main.go
@@ -15,10 +15,10 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	lintPackages(packages)
 }
 
 func lintPackages(pkgs map[string]*ast.Package) error {
-
 	for _, pkg := range pkgs {
 		for _, f := range pkg.Files {
 			for _, decl := range f.Decls {
@@ -38,7 +38,7 @@ func lintPackages(pkgs map[string]*ast.Package) error {
 						for _, spec := range genDecl.Specs {
 							valueSpec := spec.(*ast.ValueSpec)
 
-							for _, name := range valueSpec.Names {
+							for i, name := range valueSpec.Names {
 								if !ast.IsExported(name.Name) {
 									continue
 								}
@@ -46,7 +46,30 @@ func lintPackages(pkgs map[string]*ast.Package) error {
 								if name.Name != "Pkg" {
 									return fmt.Errorf("package %v exports variable %v", pkg.Name, name)
 								}
-								panic("hi")
+
+								compositeLit, ok := valueSpec.Values[i].(*ast.CompositeLit)
+								if !ok {
+									return fmt.Errorf("Pkg variable does not equal pkg")
+								}
+
+								ident, ok := compositeLit.Type.(*ast.Ident)
+								if !ok {
+									return fmt.Errorf("Pkg variable does not equal pkg")
+								}
+
+								if ident.Name != "pkg" {
+									return fmt.Errorf("Pkg variable does not equal pkg")
+								}
+
+								pkgType, ok := ident.Obj.Decl.(*ast.TypeSpec)
+								if !ok {
+									return fmt.Errorf("pkg is not a type")
+								}
+
+								if pkgType.TypeParams != nil {
+									return fmt.Errorf("pkg must not have any fields")
+								}
+
 							}
 						}
 					}
@@ -56,4 +79,5 @@ func lintPackages(pkgs map[string]*ast.Package) error {
 			}
 		}
 	}
+	return nil
 }
